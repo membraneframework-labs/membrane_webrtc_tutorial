1ab70f7451da5538f4d3e0e3e753646968c3f3d2
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2021-12-03 16:51:34 +0100

    ICE candidate inspection

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,29 @@
 // We'll put the code within an async function, so we could use await
 async function init() {
-
+  const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+  document.querySelector('#local-video').srcObject = localStream;
+  const peerConnection = new RTCPeerConnection({
+    iceServers: [
+      // { urls: 'stun:stun.l.google.com:19302' },
+      { urls: 'stun:turn.membraneframework.org:19302' },
+      {
+        urls: [
+          'turn:turn.membraneframework.org?transport=udp',
+          'turn:turn.membraneframework.org?transport=tcp',
+          'turns:turn.membraneframework.org:443?transport=tcp',
+        ],
+        username: 'turn',
+        credential: 'PASSWORD'
+      },
+    ],
+    iceTransportPolicy: 'relay'
+  });
+  localStream.getTracks().forEach(track => {
+    peerConnection.addTrack(track, localStream);
+  });
+  const offer = await peerConnection.createOffer();
+  peerConnection.onicecandidate = (event) => console.log(event.candidate);
+  await peerConnection.setLocalDescription(offer);
 }
 
 init();
