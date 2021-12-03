1de8d76a21c7c4d259d74e6f5568c55abf89c2e9
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2021-12-03 16:39:09 +0100

    Offer SDP logging

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,13 @@
 // We'll put the code within an async function, so we could use await
 async function init() {
-
+  const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  document.querySelector('#local-video').srcObject = localStream;
+  const peerConnection = new RTCPeerConnection();
+  localStream.getTracks().forEach(track => {
+    peerConnection.addTrack(track, localStream);
+  });
+  const offer = await peerConnection.createOffer();
+  console.log(offer.sdp);
 }
 
 init();
