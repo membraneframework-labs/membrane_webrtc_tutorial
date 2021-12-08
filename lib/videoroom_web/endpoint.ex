f75d03cecb4274325a95b7480bd30e91722624f7
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2021-12-08 11:21:27 +0100

    Add .formatter.exs

--- lib/videoroom_web/endpoint.ex
+++ lib/videoroom_web/endpoint.ex
@@ -1,19 +1,17 @@
 defmodule VideoRoomWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :membrane_videoroom_demo
 
-  socket("/socket", VideoRoomWeb.UserSocket,
+  socket "/socket", VideoRoomWeb.UserSocket,
     websocket: true,
     longpoll: false
-  )
 
-  plug(Plug.Static,
+  plug Plug.Static,
     at: "/",
     from: "assets",
     brotli: true,
     gzip: true
-  )
 
-  plug(Plug.Parsers,
+  plug Plug.Parsers,
     parsers: [
       :urlencoded,
       :multipart,
@@ -22,5 +20,4 @@ defmodule VideoRoomWeb.Endpoint do
     ],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
-  )
 end
